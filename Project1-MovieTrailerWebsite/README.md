9f2f3ea9899e1ce398e885a075353de77ea7cbc4
dmat1986 <39274307+dmat1986@users.noreply.github.com>
2018-05-14 13:15:48 -0400

    Add files via upload

---
+++ Project1-MovieTrailerWebsite/README.md
@@ -0,0 +1,9 @@
+# Movie Trailer Website
+
+First project in the Udacity Full Stack Engineer Nanodegree.
+
+This project stores a list of movies, including box art imagery and a movie trailer URL, and generates a static web page allowing visitors to browse movies and watch the trailers.
+
+
+**How To Run:**
+- The project can be run through the 'entertainment_center.py' file.
---
+++ Project1-MovieTrailerWebsite/entertainment_center.py
@@ -0,0 +1,38 @@
+import media
+import fresh_tomatoes
+
+goodfellas = media.Movie("Goodfellas", 
+						"The story of Henry Hill and his life in the mob",
+						"https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg",
+						"https://www.youtube.com/watch?v=xWMxvFvhAB8")
+
+
+bladerunner = media.Movie("Blade Runner (1982)", 
+					 "A movie about human replicants",
+					 "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
+					 "https://www.youtube.com/watch?v=eogpIG53Cis")
+
+
+groundhogday = media.Movie("Groundhog Day", 
+					 "A man finds himself in the same day over and over",
+					 "https://upload.wikimedia.org/wikipedia/en/b/b1/Groundhog_Day_%28movie_poster%29.jpg",
+					 "https://www.youtube.com/watch?v=GncQtURdcE4")
+
+apollo13 = media.Movie("Apollo 13", 
+					 "NASA must return Apollo 13 to Earth",
+					 "https://upload.wikimedia.org/wikipedia/en/9/9e/Apollo_thirteen_movie.jpg",
+					 "https://www.youtube.com/watch?v=Y_rkXC9HH9k")
+
+sicario = media.Movie("Sicario", 
+					 "FBI agent must aid in the drug war at US/Mexican border",
+					 "https://upload.wikimedia.org/wikipedia/en/4/4b/Sicario_poster.jpg",
+					 "https://www.youtube.com/watch?v=sR0SDT2GeFg")
+
+johnwick2 = media.Movie("John Wick 2", 
+					 "An ex-hitman must survive a large bounty on his life",
+					 "https://upload.wikimedia.org/wikipedia/en/3/31/John_Wick_Chapter_Two.png",
+					 "https://www.youtube.com/watch?v=ChpLV9AMqm4")
+
+movies = [goodfellas, bladerunner, johnwick2, groundhogday, 
+apollo13, sicario]
+fresh_tomatoes.open_movies_page(movies)
